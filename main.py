f1e93283e421adf304a80412f0cc639258d96b75
Teddy Lee <teddylee777@gmail.com>
2024-12-21 01:40:29 +0900

    main.py 파일 변경
    main.py 파일 변경

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ load_dotenv()
 # 프로젝트 이름
 logging.langsmith("Perplexity")
 
-st.title("Perplexity 💬")
+st.title("퍼플렉시티 💬")
 st.markdown(
     "LLM에 **웹검색 기능** 을 추가한 [Perplexity](https://www.perplexity.ai/) 클론 입니다. _멀티턴_ 대화를 지원합니다."
 )
